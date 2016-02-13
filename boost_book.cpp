759eca7ac2007706f382202bf9baa6f1c23a5589
Bernhard Biskup <bbiskup@gmx.de>
2016-02-13 19:35:58 +0100

    new prog_visitor

--- boost_book.cpp
+++ boost_book.cpp
@@ -1,6 +1,7 @@
 #define BOOST_SPIRIT_USE_PHOENIX_V3
 #include <boost/spirit/include/qi.hpp>
 #include <boost/phoenix/phoenix.hpp>
+#include <boost/variant.hpp>
 #include <string>
 #include <iostream>
 #include <iterator>
@@ -116,4 +117,27 @@ void prog_attribute() {
   }
 }
 
-int main() { prog_attribute(); }
+struct print : public boost::static_visitor<> {
+  template <typename T>
+  void operator()(T t) const {
+    std::cout << "t: " << std::boolalpha << t << std::endl;
+  }
+};
+
+void prog_visitor() {
+  std::string s;
+  std::getline(std::cin, s);
+  auto it = s.begin();
+  qi::rule<std::string::iterator, boost::variant<int, bool>(),
+           ascii::space_type> value = qi::int_ | qi::bool_;
+  qi::rule<std::string::iterator, std::vector<boost::variant<int, bool>>(),
+           ascii::space_type> values = value % ",";
+  std::vector<boost::variant<int, bool>> v;
+  if (qi::phrase_parse(it, s.end(), values, ascii::space, v)) {
+    for (const auto& elem : v) {
+      boost::apply_visitor(print{}, elem);
+    }
+  }
+}
+
+int main() { prog_visitor(); }
