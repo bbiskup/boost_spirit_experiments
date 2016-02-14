cc4ae767eee5f28394294bbfd5afd362a134d811
Bernhard Biskup <bbiskup@gmx.de>
2016-02-14 12:00:53 +0100

    new prog_parse_to_struct

--- boost_book.cpp
+++ boost_book.cpp
@@ -2,6 +2,7 @@
 #include <boost/spirit/include/qi.hpp>
 #include <boost/phoenix/phoenix.hpp>
 #include <boost/variant.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
 #include <string>
 #include <iostream>
 #include <iterator>
@@ -166,4 +167,49 @@ void prog_grammar() {
   }
 }
 
-int main() { prog_grammar(); }
+typedef boost::variant<int, bool> int_or_bool;
+
+struct int_or_bool_values {
+  int_or_bool first;
+  std::vector<int_or_bool> others;
+};
+
+BOOST_FUSION_ADAPT_STRUCT(int_or_bool_values,
+                          (int_or_bool, first)(std::vector<int_or_bool>,
+                                               others))
+
+template <typename Iterator, typename Skipper>
+struct my_struct_grammar
+    : qi::grammar<Iterator, int_or_bool_values(), Skipper> {
+  my_struct_grammar() : my_struct_grammar::base_type{values} {
+    value = qi::int_ | qi::bool_;
+    values = value >> ',' >> value % ',';
+  }
+
+  qi::rule<Iterator, int_or_bool(), Skipper> value;
+  qi::rule<Iterator, int_or_bool_values(), Skipper> values;
+};
+
+struct struct_print : boost::static_visitor<> {
+  template <typename T>
+  void operator()(T t) const {
+    std::cout << "struct_print: " << std::boolalpha << t << std::endl;
+  }
+};
+
+void prog_parse_to_struct() {
+  std::string s;
+  std::getline(std::cin, s);
+  auto it = s.begin();
+  my_struct_grammar<std::string::iterator, ascii::space_type> g;
+  int_or_bool_values v;
+  if (qi::phrase_parse(it, s.end(), g, ascii::space, v)) {
+    struct_print p;
+    boost::apply_visitor(p, v.first);
+    for (const auto& elem : v.others) {
+      boost::apply_visitor(p, elem);
+    }
+  }
+}
+
+int main() { prog_parse_to_struct(); }
