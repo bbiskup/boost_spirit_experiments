9f6c89f9a6e3dacb90575c3b8961884ee8b87f9e
Bernhard Biskup <bbiskup@gmx.de>
2016-02-14 12:20:52 +0100

    Removed dead code

--- c64assembler.cpp
+++ c64assembler.cpp
@@ -12,19 +12,6 @@ using std::istreambuf_iterator;
 // template <typename Iterator, typename Skipper>
 //    struct AsmGrammar : qi::grammar <
 
-// const std::string sample_prog_1{
-//     "; This program rapidly updates the colors\n"
-//     ";\n"
-//     "of the screen and the border.\n"
-//     "\n"
-//     "*= $c000     ;  starting address of the program\n"
-//     "\n"
-//     "SCREEN = $d021 BORDER = $d020\n"
-//     "\n"
-//     "start inc SCREEN  ; increase screen colour\n"
-//     "inc BORDER        ; increase border colour"
-//     "jmp start         ; repeat\n"};
-
 int main() {
   ifstream ifs{"asm_example_1.asm"};
   string prog_str{istreambuf_iterator<char>(ifs), istreambuf_iterator<char>()};
