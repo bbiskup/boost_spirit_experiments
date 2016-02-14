c38522379b487fdd0903813659e5484ac62aacf2
Bernhard Biskup <bbiskup@gmx.de>
2016-02-14 12:48:15 +0100

    Added symbol table

--- c64assembler.cpp
+++ c64assembler.cpp
@@ -9,8 +9,17 @@ using std::endl;
 using std::ifstream;
 using std::istreambuf_iterator;
 
+using namespace boost::spirit;
+
 // template <typename Iterator, typename Skipper>
 //    struct AsmGrammar : qi::grammar <
+
+typedef const char* Mnemonic;
+Mnemonic LDA = "LDA";
+
+struct Mnemonics : qi::symbols<char, Mnemonic> {
+  Mnemonics() { add("LDA", LDA); };
+};
 
 int main() {
   ifstream ifs{"asm_example_1.asm"};
