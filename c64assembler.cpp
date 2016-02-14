2614a81fa7e1e0e324517443cab73c6fc2ff4257
Bernhard Biskup <bbiskup@gmx.de>
2016-02-14 13:20:42 +0100

    Fixed compile error (was due to using qi::space_type instead of ascii::space_type

--- c64assembler.cpp
+++ c64assembler.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 
 using std::string;
+using std::boolalpha;
 using std::cout;
 using std::endl;
 using std::ifstream;
@@ -14,14 +15,17 @@ using namespace boost::spirit;
 // template <typename Iterator, typename Skipper>
 //    struct AsmGrammar : qi::grammar <
 
+const char COMMENT_CHAR{';'};
+
 typedef const char* Mnemonic;
 Mnemonic LDA = "LDA";
 Mnemonic STA = "STA";
 
 struct Mnemonics : qi::symbols<char, Mnemonic> {
   Mnemonics() {
+    // we need to add symbols dynamically
     for (const auto& elem : {LDA, STA}) {
-      add(elem, elem);
+      add(elem);  // TODO call with same elem twice, or is once equivalent?
     }
   }
 };
@@ -31,4 +35,22 @@ int main() {
   string prog_str{istreambuf_iterator<char>(ifs), istreambuf_iterator<char>()};
 
   cout << "Prog: " << prog_str << endl;
+
+  // auto it = prog_str.begin();
+  // qi::rule<std::string::iterator, qi::space_type> comment =
+  //    qi::char_(COMMENT_CHAR) >> qi::char_ >> qi::eol;
+  qi::rule<std::string::iterator, string(), ascii::space_type> comment =
+      qi::char_(COMMENT_CHAR) >> +qi::char_ >> qi::eol;
+  ;
+  string tmp = "; hier";
+  auto it = tmp.begin();
+  auto end = tmp.end();
+  string comment_str;
+  bool match = qi::phrase_parse(it, end, comment, ascii::space, comment_str);
+  // cout << "match? " << boolalpha << match << endl;
+
+  // // output remaining text
+  // if (it != end) {
+  //   std::cout << std::string{it, end} << std::endl;
+  // }
 }
