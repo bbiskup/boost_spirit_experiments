03781f27263b6c9d14bdb55db32186e68280498e
Bernhard Biskup <bbiskup@gmx.de>
2016-02-14 18:22:33 +0100

    Removed old code; renamed tentative parser

--- c64assembler.cpp
+++ c64assembler.cpp
@@ -43,16 +43,16 @@ struct CommentSkipper : qi::grammar<Iterator> {
   qi::rule<Iterator> skip;
 };
 
-// Grammar for parsing C64 assembly language
-template <typename Iterator, typename Skipper = CommentSkipper<Iterator>>
-struct AsmGrammar : public qi::grammar<Iterator, string(), Skipper> {
-  AsmGrammar() : AsmGrammar::base_type{line} {
-    line = +qi::char_ - qi::eol;
-    line.name("line");
-    qi::debug(line);
-  }
-  qi::rule<Iterator, string(), Skipper> line;
-};
+// // Grammar for parsing C64 assembly language
+// template <typename Iterator, typename Skipper = CommentSkipper<Iterator>>
+// struct AsmGrammar : public qi::grammar<Iterator, string(), Skipper> {
+//   AsmGrammar() : AsmGrammar::base_type{line} {
+//     line = +qi::char_ - qi::eol;
+//     line.name("line");
+//     qi::debug(line);
+//   }
+//   qi::rule<Iterator, string(), Skipper> line;
+// };
 
 void print_mnemo(const char* mnemo) {
   cout << "DETECTED mnemo: " << mnemo << endl;
@@ -65,21 +65,21 @@ void print_addr_spec(unsigned int addr_spec) {
   cout << "DETECTED addr_spec: " << addr_spec << endl;
 }
 
-// Fragment for testing symbol table
-template <typename Iterator, typename Skipper = CommentSkipper<Iterator>>
-struct MnemonicGrammarFragment : public qi::grammar<Iterator, Skipper> {
-  MnemonicGrammarFragment() : MnemonicGrammarFragment::base_type{mnemo} {
-    mnemo = Mnemonics[&print_mnemo];
-    mnemo.name("mnemo");
-    qi::debug(mnemo);
-  }
-  qi::rule<Iterator, Skipper> mnemo;
-};
+// // Fragment for testing symbol table
+// template <typename Iterator, typename Skipper = CommentSkipper<Iterator>>
+// struct MnemonicGrammarFragment : public qi::grammar<Iterator, Skipper> {
+//   MnemonicGrammarFragment() : MnemonicGrammarFragment::base_type{mnemo} {
+//     mnemo = Mnemonics[&print_mnemo];
+//     mnemo.name("mnemo");
+//     qi::debug(mnemo);
+//   }
+//   qi::rule<Iterator, Skipper> mnemo;
+// };
 
 // Fragment for parsing line
 template <typename Iterator, typename Skipper = CommentSkipper<Iterator>>
-struct LineGrammarFragment : public qi::grammar<Iterator, Skipper> {
-  LineGrammarFragment() : LineGrammarFragment::base_type{lines} {
+struct AsmGrammar : public qi::grammar<Iterator, Skipper> {
+  AsmGrammar() : AsmGrammar::base_type{lines} {
     line.name("line");
     lines.name("lines");
     mnemo.name("mnemo");
@@ -87,8 +87,6 @@ struct LineGrammarFragment : public qi::grammar<Iterator, Skipper> {
     instr.name("instr");
     instr_arg_immediate.name("instr_arg_immediate");
     instr_arg_absolute.name("instr_arg_absolute");
-
-    // addr_spec[&print_addr_spec];
 
     qi::debug(line);
     qi::debug(mnemo);
@@ -127,11 +125,6 @@ int main() {
   cout << "Prog: " << prog_str << endl;
   cout << "-----------------" << endl;
 
-  // auto it = prog_str.begin();
-
-  // qi::rule<std::string::iterator, string(), ascii::space_type> comment =
-  //    qi::char_(COMMENT_CHAR) >> +qi::char_ >> *qi::eol;
-  //;
   // string prog_fragment = "* = $c000; hier";
   // string prog_fragment = "LDA #$a0\nSTA $e000\nLDA $ff\n";
   string prog_fragment = "\n\nSTA $a000\nSTA $e000\nLDA $ff\n";
@@ -140,20 +133,15 @@ int main() {
   string result_str;
 
   typedef string::iterator iterator_t;
-  // typedef AsmGrammar<iterator_t> grammar;
-  typedef LineGrammarFragment<iterator_t> grammar;
+  typedef AsmGrammar<iterator_t> grammar;
   typedef CommentSkipper<iterator_t> skipper;
 
   grammar g;
   skipper sk;
 
-  // bool match = qi::phrase_parse(it, end, g, sk, result_str);
-
   while (it != end) {
     bool match = qi::phrase_parse(it, end, g, sk, qi::skip_flag::postskip);
     cout << "match? " << boolalpha << match << endl;
-    // cout << "result_str " << result_str << endl;
-
     cout << "Remaining (unparsed) text:" << endl;
     if (it != end) {
       std::cout << std::string{it, end} << std::endl;
