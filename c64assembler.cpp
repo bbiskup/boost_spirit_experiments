2e35d233f565576878d55d2a031a26abff9a76cc
Bernhard Biskup <bbiskup@gmx.de>
2016-02-14 15:49:29 +0100

    Parse mnemonic using symbol table

--- c64assembler.cpp
+++ c64assembler.cpp
@@ -21,14 +21,14 @@ typedef const char* Mnemonic;
 Mnemonic LDA = "LDA";
 Mnemonic STA = "STA";
 
-struct Mnemonics : qi::symbols<char, Mnemonic> {
-  Mnemonics() {
+struct Mnemonics_ : qi::symbols<char, Mnemonic> {
+  Mnemonics_() {
     // we need to add symbols dynamically
     for (const auto& elem : {LDA, STA}) {
       add(elem);  // TODO call with same elem twice, or is once equivalent?
     }
   }
-};
+} Mnemonics;
 
 // Custom skipper for comments in assembly language
 // see http://stackoverflow.com/a/8534840 about customer skippers
@@ -48,6 +48,15 @@ struct AsmGrammar : public qi::grammar<Iterator, string(), Skipper> {
   qi::rule<Iterator, string(), Skipper> line;
 };
 
+// Fragment for testing symbol table
+template <typename Iterator, typename Skipper = CommentSkipper<Iterator>>
+struct MnemonicGrammarFragment : public qi::grammar<Iterator, Skipper> {
+  MnemonicGrammarFragment() : MnemonicGrammarFragment::base_type{mnemo} {
+    mnemo = Mnemonics;
+  }
+  qi::rule<Iterator, Skipper> mnemo;
+};
+
 int main() {
   ifstream ifs{"asm_example_1.asm"};
   string prog_str{istreambuf_iterator<char>(ifs), istreambuf_iterator<char>()};
@@ -60,18 +69,20 @@ int main() {
   // qi::rule<std::string::iterator, string(), ascii::space_type> comment =
   //    qi::char_(COMMENT_CHAR) >> +qi::char_ >> *qi::eol;
   //;
-  string prog_fragment = "abc; hier";
+  string prog_fragment = "LDA; hier";
   auto it = prog_fragment.begin();
   auto end = prog_fragment.end();
   string result_str;
 
   typedef string::iterator iterator_t;
-  typedef AsmGrammar<iterator_t> grammar;
+  // typedef AsmGrammar<iterator_t> grammar;
+  typedef MnemonicGrammarFragment<iterator_t> grammar;
   typedef CommentSkipper<iterator_t> skipper;
 
   grammar g;
   skipper sk;
 
+  // bool match = qi::phrase_parse(it, end, g, sk, result_str);
   bool match = qi::phrase_parse(it, end, g, sk, result_str);
   cout << "match? " << boolalpha << match << endl;
   cout << "result_str " << result_str << endl;
